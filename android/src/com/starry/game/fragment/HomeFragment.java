d7a47802bb357c4b5d8082094cf173b025f39c56
kimyounghun1 <hunhoohunhoo1@gmail.com>
2019-06-11 02:52:53 +0900

    다시

--- android/src/com/starry/game/fragment/HomeFragment.java
+++ android/src/com/starry/game/fragment/HomeFragment.java
@@ -17,8 +17,10 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.starry.game.AndroidLauncher;
+import com.starry.game.Ball.BallManager;
 import com.starry.game.LoginActivity;
 import com.starry.game.MainActivity;
+import com.starry.game.ModeActivity;
 import com.starry.game.R;
 import com.starry.game.model.UserModel;
 
@@ -32,12 +34,11 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-
         Button startGame = (Button) view.findViewById(R.id.start_button);
         startGame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), AndroidLauncher.class);
+                Intent intent = new Intent(getActivity(), ModeActivity.class);
                 startActivity(intent);
             }
         });
